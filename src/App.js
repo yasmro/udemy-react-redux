e3c852e1f0c36f94d05aaf8ad4e78a71a033e6c3
Yu Ohno <kantillo0683@gmail.com>
2020-09-29 01:52:12 +0900

    first commit about props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10
+    },
+    {
+      name: "Yu",
+      age: 23
+    },
+    {
+      name: "Shuya"
+    },
+  ]
   return (
     <div>
       <h1>Hello world.</h1>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("I am Clicked.")}} />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, idx) => {
+          return <User key={idx} name={profile.name} age={profile.age} />;
+        })
+      }
     </div>
   );
 }
 
-const Cat = () => {
-  return <div>MEOW!</div>
+const User = (props) => {
+  return <div>Hello, I'm {props.name}, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 0
 }
 
 export default App;
