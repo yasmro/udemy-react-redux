3dc44843078622db1bbc2eaca1b74221947a7c01
Yu Ohno <kantillo0683@gmail.com>
2020-09-29 01:58:32 +0900

    first commit: prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
@@ -32,8 +33,9 @@ const User = (props) => {
   return <div>Hello, I'm {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 0
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
