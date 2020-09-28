f3b3e5dfb8bfbbd547a6652aa7cc4b599f4c82db
Yu Ohno <kantillo0683@gmail.com>
2020-09-29 01:33:17 +0900

    modified jsx syntax

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <h1>Hello world.</h1>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am Clicked.")}} />
     </div>
   );
 }
