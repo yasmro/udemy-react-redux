77e478fe5186973db13d7236dfe469bd0189dcb7
Yu Ohno <kantillo0683@gmail.com>
2020-09-29 01:42:53 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Hello world.</h1>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("I am Clicked.")}} />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
   );
+}
+
+const Cat = () => {
+  return <div>MEOW!</div>
 }
 
 export default App;
